bcef52ceb084b0d56d34d3022424ff2e123a3af8
Said Faizetdinov <saidfraizik@gmail.com>
2024-08-17 12:52:39 +0300

    Вычитание двух чисел

--- lessons.c
+++ lessons.c
@@ -5,7 +5,7 @@ int main(void)
     int digit_1;
     int digit_2;
     int result = scanf("%d %d", &digit_1, &digit_2);
-    int res = digit_1 + digit_2;
-    printf("answer is: %d", res);
+    int res = digit_1 - digit_2;
+    printf("The answer is: %d", res);
     return 0;
 }
